bb38b5b0a0570f44d535628efd0f712235feef54
Ricardo Canastro <ricardocanastro@gmail.com>
2024-05-03 08:25:45 +0100

    feat: add image placeholder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from "astro-imagetools";
 
 import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte(), astroImageTools]
 });
